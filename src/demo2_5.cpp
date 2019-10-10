2ae27b5979e4182d2e19533bf6d261102791d4f3
xiang <xiangkun@tju.edu.cn>
2019-10-10 21:19:05 +0800

    加上补点操作

--- src/demo2_5.cpp
+++ src/demo2_5.cpp
@@ -17,7 +17,10 @@
 #include <opencv2/calib3d/calib3d.hpp>
 using namespace std;
 
-const int MaxConer = 200;//最大角点数
+const int MAXCONER = 200;//最大角点数
+
+int grid = 100;//grid的size
+int num_point = 5;//每个grid的点数
 cv::Mat this_image, prev_image;//图片信息
 cv::Mat fast_image;
 vector<cv::Point2f> points_prev, points_this;//存储采集到的角点,上一帧、当前帧
@@ -44,13 +47,39 @@ void getPoints(cv::Mat image,vector<cv::Point2f> &points,cv::InputArray mask=cv:
      */
 
     if (maxConer > 0){
-        for (int i = 0; i < maxConer; ++i) {
+        for (int i = 0; i < maxConer && i< points_Fast.size(); ++i) {
             points.push_back(points_Fast[i].pt);
         }
     }
     if (maxConer < 0){
         for (auto kp:points_Fast)
             points.push_back(kp.pt);
+    }
+
+}
+//对传入的图片提取角点,重载，KeyPoint版
+void getPoints(cv::Mat image,vector<cv::KeyPoint> &points,cv::InputArray mask=cv::noArray(),int maxConer= -1){
+    vector<cv::KeyPoint> points_Fast; //暂时存储提取的点
+    //Fast检测器
+    cv::Ptr<cv::FastFeatureDetector> FastDetector = cv::FastFeatureDetector::create(20, true);
+    FastDetector -> detect(image, points_Fast,mask);
+    //按照响应值降序排序
+    sort(points_Fast.begin(),points_Fast.end(),compare);
+
+    /*决定导入点的数量
+     * maxConer > 0,按照maxConer导入
+     * maxConer = 0,不导入
+     * maxConer < 0,无限制
+     */
+
+    if (maxConer > 0){
+        for (int i = 0; i < maxConer; ++i) {
+            points.push_back(points_Fast[i]);
+        }
+    }
+    if (maxConer < 0){
+        for (auto kp:points_Fast)
+            points.push_back(kp);
     }
 
 }
@@ -84,6 +113,50 @@ void getPoints_grid(cv::Mat image, int grid_size,int num_point_grid, vector<cv::
     cv::Mat mask = cv::Mat::zeros(image.size(),CV_8UC1);
     mask.colRange(grid_size*n ,width).rowRange(grid_size*m,height).setTo(255);
     getPoints(image,points,mask,num_point_grid);
+}
+//按照grid提取角点,重载,加入总点数限制
+void getPoints_grid(cv::Mat image, int grid_size,int num_point_grid, vector<cv::Point2f> &points, int maxConer){
+    int width = image.cols;
+    int height = image.rows;
+    int n = width / grid_size; // 一行的grid数；
+    int m = height / grid_size; // 一列的grid数
+    vector<cv::KeyPoint> points_temp;
+    //每个grid检测
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < m; ++j) {
+            cv::Mat mask = cv::Mat::zeros(image.size(),CV_8UC1);
+            //每块grid检测，并选出最佳5个
+            mask.colRange(grid_size*i,grid_size*(i+1)).rowRange(grid_size*j,grid_size*(j+1)).setTo(255);
+            getPoints(image,points_temp,mask,num_point_grid);
+        }
+    }
+    //grid之外的边缘区域,列边缘，行边缘
+    for (int k = 0; k < n; ++k) {
+        cv::Mat mask = cv::Mat::zeros(image.size(),CV_8UC1);
+        mask.colRange(grid_size*k ,grid_size*(k+1)).rowRange(grid_size*m,height).setTo(255);
+        getPoints(image,points_temp,mask,num_point_grid);
+    }
+    for (int l = 0; l < m; ++l) {
+        cv::Mat mask = cv::Mat::zeros(image.size(),CV_8UC1);
+        mask.colRange(grid_size*n ,width).rowRange(grid_size*l,grid_size*(l+1)).setTo(255);
+        getPoints(image,points_temp,mask,num_point_grid);
+    }
+
+    cv::Mat mask = cv::Mat::zeros(image.size(),CV_8UC1);
+    mask.colRange(grid_size*n ,width).rowRange(grid_size*m,height).setTo(255);
+    getPoints(image,points_temp,mask,num_point_grid);
+
+    sort(points_temp.begin(),points_temp.end(),compare);
+    if (maxConer > 0){
+        for (int i = 0; i<maxConer && i<points_temp.size(); ++i) {
+            points.push_back(points_temp[i].pt);
+        }
+    }
+    if (maxConer < 0){
+        for (auto kp:points_temp)
+            points.push_back(kp.pt);
+    }
+
 }
 //画出grid
 void drawGrid(cv::Mat &image, int grid_size){
@@ -158,9 +231,6 @@ int main(int argc, char** argv){
         this_image.copyTo(fast_image);
         cv::cvtColor(this_image, this_image, cv::COLOR_BGR2GRAY);
 
-        //确定grid相关参数
-        int grid = 100;
-        int num_point = 5;//每个grid的点数
 
         //第一帧提取角点
         if (isfrist){
@@ -184,12 +254,22 @@ int main(int argc, char** argv){
             reducePoints(points_prev,status_Fast);
             rejectWithF(points_prev,points_this);//去除outlier
 
-            if(points_this.size() == 0) {
-                cout<<"all points are loss"<<endl;
-                break;
+            //补点操作
+            int num_add = MAXCONER - points_this.size();
+            if(num_add > 0){
+                getPoints_grid(this_image,grid,num_point,points_this,num_add);
             }
 
-            drawTrace(points_prev,points_this,fast_image);
+            if(points_this.size() == 0) {
+                cout<<"all points are loss"<<endl;
+                return 0;
+            }
+            //绘制点
+            for (int i = 0; i < points_this.size(); ++i) {
+                cv::circle(fast_image, points_this[i], 3, cv::Scalar(0, 255, 0), -1, 8);//画出点
+            }
+            //drawTrace(points_prev,points_this,fast_image);
+
         }
 
 
